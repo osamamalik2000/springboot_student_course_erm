54f7d4ae9ba86a868fcbb7f8cb8cae35c91a8737
osamamalik2000 <itsosamamalik@gmail.com>
2024-02-03 13:48:26 +0530

    minor

--- src/main/java/com/pcf/pcf_demo/controllers/CourseController.java
+++ src/main/java/com/pcf/pcf_demo/controllers/CourseController.java
@@ -1,7 +1,6 @@
 package com.pcf.pcf_demo.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
